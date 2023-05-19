55e9b597678dc034f504925872d929c05fddac09
SurajM25 <surajmanjunath2@gmail.com>
2023-05-19 14:07:20 +0530

    Added CRUD operations on post, user auth

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -13,9 +14,14 @@ class Post(models.Model):
     def __str__(self):
         return self.title
     
+    def get_absolute_url(self):
+        return reverse('post-detail',kwargs={'pk':self.pk})
+    
 class Comments(models.Model):
     post_id = models.ForeignKey(Post,on_delete=models.CASCADE)
     comment = models.TextField()
 
     def __str__(self):
         return self.comment
+    
+    
